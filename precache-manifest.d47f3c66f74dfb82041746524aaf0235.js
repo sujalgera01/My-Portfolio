a588b247a4778885802313ced727a8a999384360
sujalgera01 <sujalgera01@gmail.com>
2022-05-23 14:35:07 +0530

    Updates

---
+++ precache-manifest.d47f3c66f74dfb82041746524aaf0235.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "c60702489b87dece2645",
+    "url": "/My-Portfolio/static/css/main.988dd51b.chunk.css"
+  },
+  {
+    "revision": "c60702489b87dece2645",
+    "url": "/My-Portfolio/static/js/main.c6070248.chunk.js"
+  },
+  {
+    "revision": "064ab7e55fb29fa1c3e8",
+    "url": "/My-Portfolio/static/js/1.064ab7e5.chunk.js"
+  },
+  {
+    "revision": "aa9d0237064e9f092376",
+    "url": "/My-Portfolio/static/js/runtime~main.aa9d0237.js"
+  },
+  {
+    "revision": "7c0add1f2802f74692b56816dedf684c",
+    "url": "/My-Portfolio/static/media/resume.7c0add1f.pdf"
+  },
+  {
+    "revision": "a9bc3404469e5a540ec787eeab632085",
+    "url": "/My-Portfolio/static/media/profile.a9bc3404.jpg"
+  },
+  {
+    "revision": "238933a0980eac094cee183909e38e4d",
+    "url": "/My-Portfolio/static/media/about.238933a0.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/My-Portfolio/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/My-Portfolio/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/My-Portfolio/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/My-Portfolio/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/My-Portfolio/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/My-Portfolio/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "6f3f7ce5392233ad3eeb4c4d9d57ddf3",
+    "url": "/My-Portfolio/index.html"
+  }
+];
